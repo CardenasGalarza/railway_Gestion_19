577e634d6eb6e01b49096cc3812d8d724a13d8ad
CardenasGalarza <alex10estadistica@gmail.com>
2022-11-07 13:57:39 -0500

    rwer

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import time
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -21,3 +23,18 @@ driver = webdriver.Firefox(
     service=service,
 )
 driver.get('https://auth.movistaradvertising.com/login?logout')
+
+
+
+username = 'caramburu_TDP'
+passwordd = 'WebSys29*T*'
+
+
+xpath = driver.find_element_by_xpath('//INPUT[@id="username"]')
+xpath.send_keys(username)
+time.sleep(2)
+
+xpath = driver.find_element_by_xpath('//INPUT[@id="password"]')
+xpath.send_keys(passwordd)
+time.sleep(2)
+
