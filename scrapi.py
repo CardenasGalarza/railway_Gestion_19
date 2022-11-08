9b053f7c0e9d321ea81ad64b34ee9632a9c6cc70
CardenasGalarza <alex10estadistica@gmail.com>
2022-11-08 00:10:19 -0500

    wxw

--- scrapi.py
+++ scrapi.py
@@ -31,6 +31,7 @@ driver.execute_cdp_cmd('Page.setDownloadBehavior', params)
 
 
 
+
 username = 'caramburu_TDP'
 passwordd = 'WebSys29*T*'
 
@@ -38,49 +39,49 @@ passwordd = 'WebSys29*T*'
 driver.get("https://auth.movistaradvertising.com/login?logout")
 time.sleep(3)
 
-xpath = driver.find_element_by_xpath('//INPUT[@id="username"]')
+
+xpath = driver.find_element("xpath", '//INPUT[@id="username"]')
 xpath.send_keys(username)
 time.sleep(2)
 
-xpath = driver.find_element_by_xpath('//INPUT[@id="password"]')
+xpath = driver.find_element("xpath", '//INPUT[@id="password"]')
 xpath.send_keys(passwordd)
 time.sleep(2)
 
-xpath = driver.find_element_by_xpath('//BUTTON[@type="submit"][text()="Ingresar"]')
+xpath = driver.find_element("xpath", '//BUTTON[@type="submit"][text()="Ingresar"]')
 xpath.click()
 time.sleep(3)
 
 
-xpath = driver.find_element_by_xpath('//*[@id="dropdown-user-menu"]/div/button[2]')
+xpath = driver.find_element("xpath", '//*[@id="dropdown-user-menu"]/div/button[2]')
 xpath.click()
 time.sleep(7)
 
-xpath = driver.find_element_by_xpath('//SPAN[@_ngcontent-c1=""][text()="SMSi"]')
+xpath = driver.find_element("xpath", '//SPAN[@_ngcontent-c1=""][text()="SMSi"]')
 xpath.click()
 time.sleep(6)
 
 celu = '925266696'
 mensaje = 'prueba77'
 
-xpath = driver.find_element_by_xpath('//INPUT[@id="inputGsmList"]')
+xpath = driver.find_element("xpath", '//INPUT[@id="inputGsmList"]')
 xpath.send_keys(celu)
 time.sleep(6)
 
-xpath = driver.find_element_by_xpath('//TEXTAREA[@id="txtMessage"]')
+xpath = driver.find_element("xpath", '//TEXTAREA[@id="txtMessage"]')
 xpath.send_keys(mensaje)
 time.sleep(6)
 
 
-xpath = driver.find_element_by_xpath('//BUTTON[@id="buttonProcess"]')
+xpath = driver.find_element("xpath", '//BUTTON[@id="buttonProcess"]')
 xpath.click()
 time.sleep(5)
 
-xpath = driver.find_element_by_xpath('//*[@id="buttonSend"]')
+xpath = driver.find_element("xpath", '//*[@id="buttonSend"]')
 xpath.click()
 time.sleep(5)
 
 driver.quit()
-
 
 
 
