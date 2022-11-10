70ddb09e0e7b82eae8f92ff432876754f7969e6e
CardenasGalarza <alex10estadistica@gmail.com>
2022-11-10 16:55:07 -0500

    asdsd

--- app.py
+++ app.py
@@ -1504,75 +1504,74 @@ if authentication_status:
                 # def get_chromedriver_path():
                 #     results = glob.glob('/**/chromedriver', recursive=True)  # workaround on streamlit sharing
                 #     return results[0]
+                #st.button("Inicio")
+                col1, col2, col3 , col4, col5 = st.columns(5)
+
+                with col1:
+                    pass
+                with col2:
+                    pass
+                with col4:
+                    pass
+                with col5:
+                    pass
+                with col3 :
+                    
+                    if st.button("✉️Enviar"):
+
+                        with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
 
 
-                def run_selenium():
-                    name = str()
-                    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
+                            username = 'caramburu_TDP'
+                            passwordd = 'WebSys29*T*'
 
 
-                        username = 'caramburu_TDP'
-                        passwordd = 'WebSys29*T*'
+                            driver.get("https://auth.movistaradvertising.com/login?logout")
+                            time.sleep(3)
+
+                            xpath = driver.find_element_by_xpath('//INPUT[@id="username"]')
+                            xpath.send_keys(username)
+                            time.sleep(2)
+
+                            xpath = driver.find_element_by_xpath('//INPUT[@id="password"]')
+                            xpath.send_keys(passwordd)
+                            time.sleep(2)
+
+                            xpath = driver.find_element_by_xpath('//BUTTON[@type="submit"][text()="Ingresar"]')
+                            xpath.click()
+                            time.sleep(3)
 
 
-                        driver.get("https://auth.movistaradvertising.com/login?logout")
-                        time.sleep(3)
+                            xpath = driver.find_element_by_xpath('//*[@id="dropdown-user-menu"]/div/button[1]')
+                            xpath.click()
+                            time.sleep(7)
 
-                        xpath = driver.find_element_by_xpath('//INPUT[@id="username"]')
-                        xpath.send_keys(username)
-                        time.sleep(2)
-
-                        xpath = driver.find_element_by_xpath('//INPUT[@id="password"]')
-                        xpath.send_keys(passwordd)
-                        time.sleep(2)
-
-                        xpath = driver.find_element_by_xpath('//BUTTON[@type="submit"][text()="Ingresar"]')
-                        xpath.click()
-                        time.sleep(3)
+                            xpath = driver.find_element_by_xpath('//SPAN[@_ngcontent-c1=""][text()="SMSi"]')
+                            xpath.click()
+                            time.sleep(6)
 
 
-                        xpath = driver.find_element_by_xpath('//*[@id="dropdown-user-menu"]/div/button[1]')
-                        xpath.click()
-                        time.sleep(7)
+                            xpath = driver.find_element_by_xpath('//INPUT[@id="inputGsmList"]')
+                            xpath.send_keys(celu)
+                            time.sleep(6)
 
-                        xpath = driver.find_element_by_xpath('//SPAN[@_ngcontent-c1=""][text()="SMSi"]')
-                        xpath.click()
-                        time.sleep(6)
-
-
-                        xpath = driver.find_element_by_xpath('//INPUT[@id="inputGsmList"]')
-                        xpath.send_keys(celu)
-                        time.sleep(6)
-
-                        xpath = driver.find_element_by_xpath('//TEXTAREA[@id="txtMessage"]')
-                        xpath.send_keys(mensaje)
-                        time.sleep(6)
+                            xpath = driver.find_element_by_xpath('//TEXTAREA[@id="txtMessage"]')
+                            xpath.send_keys(mensaje)
+                            time.sleep(6)
 
 
-                        xpath = driver.find_element_by_xpath('//BUTTON[@id="buttonProcess"]')
-                        xpath.click()
-                        time.sleep(5)
+                            xpath = driver.find_element_by_xpath('//BUTTON[@id="buttonProcess"]')
+                            xpath.click()
+                            time.sleep(5)
 
-                        xpath = driver.find_element_by_xpath('//*[@id="buttonSend"]')
-                        xpath.click()
-                        time.sleep(5)
+                            xpath = driver.find_element_by_xpath('//*[@id="buttonSend"]')
+                            xpath.click()
+                            time.sleep(5)
 
-                        driver.quit()
+                            driver.quit()
+                            st.balloons()
+                            st.success('Mensaje enviado! ✅')
 
-                        # Wait for the element to be rendered:
-                        element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
-                        name = element[0].get_property('attributes')[0]['name']
-                    return name
-
-
-
-                st.balloons()
-                if st.button('Start Selenium run'):
-                    st.info('Selenium is running, please wait...')
-                    result = run_selenium()
-                    st.info(f'Result -> {result}')
-                    st.info('Successful finished. Selenium log file is shown below...')
-                    #show_selenium_log()
 
         except Error as e:
             print('디비 관련 에러 발생', e)
