2b22813789cf209ec32fb5a5a3b69cdc044f53aa
CardenasGalarza <alex10estadistica@gmail.com>
2022-11-10 16:31:34 -0500

    sadsd

--- app.py
+++ app.py
@@ -1479,7 +1479,7 @@ if authentication_status:
                 from selenium.webdriver.common.by import By
 
                 options = Options()
-                #options.add_argument("--headless")
+                options.add_argument("--headless")
                 options.add_argument("--no-sandbox")
                 options.add_argument("--disable-dev-shm-usage")
                 options.add_argument("--disable-gpu")
